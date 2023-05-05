05c34fba426173e9c992de16eeb9290d3f5162d0
001subirsingh <thesubir@gmail.com>
2023-05-06 01:59:16 +0530

    add

--- main.py
+++ main.py
@@ -1,26 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import json
+from datetime import datetime
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+locale_server = True
 
 app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
+)
+
+mail = Mail(app)
+
+if locale_server:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['local_uri']
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['prod_uri']
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False)
+    email = db.Column(db.String(20), unique=True)
+    phone_num = db.Column(db.String(120), unique=True)
+    msg = db.Column(db.String(120), unique=True)
+    date = db.Column(db.String(120), unique=True)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('msg')
+        entry = Contact(name=name, email=email, phone_num=phone, msg=message, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+        mail.send_message('New message from ' + name,
+                          sender='email',
+                          recipients=[params['gmail-user']],
+                          body=message + "\n" + phone)
+
+    return render_template('contact.html', params=params)
 
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 
 app.run(debug=True)
